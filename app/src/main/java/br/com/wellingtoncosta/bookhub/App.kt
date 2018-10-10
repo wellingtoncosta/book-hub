f53412c80b284abb62a533a5c6431ac8c5897456
Wellington Costa <wellington.costa128@gmail.com>
2018-10-09 21:27:11 -0300

    Starting Koin modules.

---
+++ app/src/main/java/br/com/wellingtoncosta/bookhub/App.kt
@@ -0,0 +1,16 @@
+package br.com.wellingtoncosta.bookhub
+
+import android.app.Application
+import br.com.wellingtoncosta.bookhub.di.bookHubApp
+import org.koin.android.ext.android.startKoin
+
+/**
+ * @author Wellington Costa on 09/10/18.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, bookHubApp)
+    }
+}
